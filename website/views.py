bba1d8ee43ca978ab50787c687f661aef00cb5e4
borbapugliese18 <borbapugliese@hotmail.com>
2024-06-21 18:24:37 -0300

    estilizar record

--- website/views.py
+++ website/views.py
@@ -1,13 +1,49 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-
+from website.models import Record
 
 def home(request):
-    return render(request, 'home.html', {})
+    if not request.user.is_authenticated:
+        messages.error(request, 'Usuário não logado')
+        return redirect('login')
+    
+    records = Record.objects.all()
+
+    
+    return render(request, 'home.html', {'records':records})
 
 def login_user(request):
-    pass
+
+    if request.method == 'POST': 
+        username = request.POST['username']
+        password = request.POST['password']
+
+        usuario = authenticate(
+            request, 
+            username=username,
+            password=password
+        )
+        if usuario is not None:
+            login(request, usuario)
+            messages.success(request, "Login efetuado com sucesso!")
+            return redirect('home')
+        else:
+            messages.error(request, 'Erro ao efetuar login')
+            return redirect('login')
+
+    return render(request, 'login.html', {})
 
 def logout_user(request):
-    pass
+    logout(request)
+    messages.success(request, "Logout efetuado com sucesso!")
+    return redirect('login')
+
+def customer_record(request, pk):
+    if not request.user.is_authenticated:
+        messages.error(request, 'Usuário não logado')
+        return redirect('login')
+    
+    customer_record = Record.objects.get(id=pk)
+    
+    return render(request, 'record.html', {'customer_record':customer_record})
